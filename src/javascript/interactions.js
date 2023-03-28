827d905067deaa4a125cc9f954111902ed86ba18
Albert Kovtoun <kovtoun.albert@hotmail.com>
2023-03-28 17:32:07 +0200

    :sparkles: Added clipboard copy

--- src/javascript/interactions.js
+++ src/javascript/interactions.js
@@ -1,4 +1,14 @@
-function openInNewTab(url) {
-  //   window.open(url, "_blank").focus()
-  window.open(url)
-}
+const contactUsEmail = document.querySelector(".contact-us-email")
+const tooltip = document.querySelector(".tooltip")
+
+contactUsEmail.addEventListener("click", function () {
+  let copyText = document.querySelector(".contact-us-email")
+
+  tooltip.classList.add("fade-in-out")
+
+  setTimeout(() => {
+    tooltip.classList.remove("fade-in-out")
+  }, 2000)
+
+  navigator.clipboard.writeText(copyText.innerHTML)
+})
